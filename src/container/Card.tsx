1a3856a6bc6aaaf8f93d899aef156756fbfabcd3
Sandeep Edara <edarasp@mail.uc.edu>
2023-06-29 13:03:33 -0400

    card container

---
+++ src/container/Card.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+const Card = ({ children }: { children: ReactNode}) => {
+    return (
+        <div className="p-5 mt-4 rounded-xl shadow-md flex items-center text-left">
+        {children}
+        </div>
+    );
+}
+
+export default Card;
