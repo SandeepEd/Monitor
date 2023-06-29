c7902c63600d830d9880557afaaac773b5834b62
Sandeep Edara <edarasp@mail.uc.edu>
2023-06-29 15:30:31 -0400

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import AudioInputComponent from './components/AudioInput'
 import HardwareStatus from './components/HardwareStatus'
+import NavBar from './components/Navbar'
 import VideoInputComponent from './components/VideoInput'
 
 function App() {
 
   return (
     <>
+    <NavBar />
       <HardwareStatus />
       <VideoInputComponent />
       <AudioInputComponent />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <div className="border-2 flex p-4 items-center justify-center text-center rounded-lg border-white">
+            <span className="font-bold text-xl">Matrox Monarch Status</span>
+        </div>
+    );
+}
+
+export default NavBar;
