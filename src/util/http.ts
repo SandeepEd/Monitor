d7dd898c976708408997f3e5712a90665b3a2d09
Sandeep Edara <edarasp@mail.uc.edu>
2023-06-30 01:26:45 -0400

    use proxy

--- src/util/http.ts
+++ src/util/http.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: `http://admin:t3st1u1999@129.79.38.39/Monarch/syncconnect/sdk.aspx`,
+  baseURL: `/api`,
   withCredentials: true,
   headers: {
     "Content-Type": `application/json`,
     "credentials": `include`,
+  },
+  auth: {
+    username: 'admin',
+    password: 't3st1u1999'
   },
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://admin:t3st1u1999@129.79.38.39/Monarch/syncconnect/sdk.aspx',
+      },
+    },
+  },
   plugins: [react()],
 })
